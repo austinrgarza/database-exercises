9cd7ebcbd3c6c657dc6d8535ea764e92b6184e37
Austin <Austin.r.garza@gmail.com>
2019-01-29 09:41:24 -0600

    + delete exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,5 @@
 DROP TABLE if EXISTS albums;
 CREATE TABLE albums (
-
     id int UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(300) NOT NULL default 'NONE',
     record_name VARCHAR (300) NOT NULL ,
---
+++ delete_exercise.sql
@@ -0,0 +1,3 @@
+DELETE * FROM albums WHERE release_date > 1991;
+DELETE * FROM albums WHERE genre like '%Disco%';
+DELETE * FROM albums WHERE artist_name like '%Whitney Houston%';
