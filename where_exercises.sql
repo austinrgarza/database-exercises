95028dbc198ab12f7241f37b9ec6ecb965551044
Austin <Austin.r.garza@gmail.com>
2019-01-29 13:36:07 -0600

    +updated where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,43 @@
 USE employees;
 
-SELECT * from employees;
+SELECT *
+FROM employees
+     # WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Irenta'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya')
+  AND gender = 'M';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+SELECT *
+FROM employees
+WHERE last_name like 'E%' OR last_name like '%E';
+
+SELECT *
+FROM employees
+WHERE last_name like 'E%E';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+Where birth_date like '%-12-31';
+
+SELECT *
+FROM employees
+WHERE last_name like '%q%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date like '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name like '%Q%'
+  AND last_name NOT LIKE '%QU%';
