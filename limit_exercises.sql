b9af25e4981b38a8bdf04a765af3eda584bc05ea
Austin <Austin.r.garza@gmail.com>
2019-01-29 14:43:42 -0600

    + limit/order exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,22 @@
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT hire_date,first_name, last_name, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 50;
+
+
---
+++ order_by_exercises.sql
@@ -0,0 +1,40 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no ASC;
+
+SELECT birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT hire_date,first_name, last_name, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
